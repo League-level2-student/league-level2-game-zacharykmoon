3abd409347ccd478e350f3b27c89646c7380f8bd
league <league@imac.attlocal.net>
2023-06-28 18:30:58 -0700

    jj

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
@@ -6,7 +7,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
-
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
@@ -14,200 +15,99 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-
-
-public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	
-	private static final long serialVersionUID = 1L;
-	public static BufferedImage image;
-	public static boolean needImage = true;
-	public static boolean gotImage = false;
-	final int MENU = 0;
-	
-	int currentState = MENU;
-	ObjectManager manager= new ObjectManager();
-	final int GAME = 1;
-	final int END = 2;
-	Font titleFont;
-	Font gameFont;
-	Font gameInstructions;
 
-	Timer zombieSpawn;
-
-	Timer frameDraw;
-	void updateMenuState() {}
-	void updateGameState() {
-		manager.update();
-		if(manager.peashooter.active==false) {
-			currentState=END;
+public class GamePanel extends JPanel implements Runnable, KeyListener {
+private static final long serialVerisonUID = 1L;
+	public static final int WIDTH = 500, HEIGHT = 500;
+	private Thread thread;
+	private boolean running;
+	 private boolean right = true, left = false, up = false, down = false;{
+		 
+	 }
+	private Peashooter b;
+	private ArrayList<Peashooter> snake;
+	private int xCoor = 10, yCoor = 10, size= 5;
+	private int tick = 0;
+		public  GamePanel()	{
+			setPreferredSize(new Dimension(WIDTH,HEIGHT));
+			start();
 		}
-	}
-	void loadImage(String imageFile) {
-	    if (needImage) {
-	        try {
-	            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
-		    gotImage = true;
-	        } catch (Exception e) {
-	            
-	        }
-	        needImage = false;
-	    }
-	}
-	GamePanel (){
-		titleFont = new Font("Arial", Font.PLAIN, 48);
-		gameFont = new Font("Arial", Font.PLAIN, 10);
-		gameInstructions = new Font("Arial", Font.PLAIN, 28);
-		// frameDraw = new Timer(1000/60, this);
-		frameDraw = new Timer(1000 / 60, this);
-		frameDraw = new Timer(1000/6,this);
-		frameDraw.start();
-	
-		if (needImage) {
-		    loadImage("grass.png");
-		}
-	}
-	    
-	
-	
-		void startGame() {
-		    zombieSpawn = new Timer(1000/6, this);
-		    zombieSpawn.start();
-		}
+ 
+public void start(){
+	running = true;
+	thread = new Thread(this);
+thread.start();
 
-void endGame() {
-	zombieSpawn.stop();
 }
-		
-		
-		void updateEndState() {}
-		void drawGameState(Graphics g) {
-			if (gotImage) {
-				g.drawImage(image, EatingPlant.WIDTH, EatingPlant.HEIGHT, null);
-			} else {
-				g.setColor(Color.BLACK);
-				g.fillRect(0, 0, EatingPlant.WIDTH,EatingPlant.HEIGHT);
-			}
-			
-			manager.draw(g);
-			
-			//System.out.println("Rocket");
-			
-		}
-		void drawMenuState(Graphics g) {
-			g.setColor(Color.BLUE);
-			g.fillRect(0, 0, EatingPlant.WIDTH, EatingPlant.HEIGHT);
-			g.setColor(Color.YELLOW);
-			g.setFont(titleFont);
-			g.drawString("Hello neighbor, use peashoooter to fight off the zombies", 150, 250);
-			g.drawString("Press Enter to start", 50, 300);
-			g.drawString("Press space for info", 50, 500);
-		}
-		void drawEndState(Graphics g) {
-			g.setColor(Color.RED);
-			g.fillRect(0, 0, EatingPlant.WIDTH, EatingPlant.HEIGHT);
-			g.setColor(Color.YELLOW);
-			g.setFont(titleFont);
-			g.drawString("Game Over", 150, 250);
-			g.drawString("You kill "+ manager.getScore() +" many enimies", 50, 300);
-			g.drawString("Press ENTER to restart", 50, 400);
-		}
-		
+	
 
+public void stop(){
+	running = false;
+	try {
+	thread.join();
+} catch(InterruptedException e) {
+	e.printStackTrace();
+	
+}
+}
+public void tick(){
+	if(snake.size() --0) {
+		b = new Peashooter(xCoor, yCoor, 10);
+		snake.add(b);
+}
+ticks++;
+if(ticks > 250000) {
+	if(right) xCoor++;
+if(left) xCoor --;
+if(up) yCoor--;  
+if(down) yCoor++;
+ ticks = 0;
+ b = new Peashooter(xCoor, yCoor, 10);
+ snake.add(b);
+ if(snake.size()> size)
+	 snake.remove(0);
+}
+}
+}
+public void paint(Graphics g){
+	
+	g.clearRect(0, 0, WIDTH, HEIGHT);
+	g.fillRect(0, 0, WIDTH, HEIGHT);
+g.setColor(Color.BLACK);
 
-		
-		@Override
-		public void keyPressed(KeyEvent e) {
-			// TODO Auto-generated method stub
-			System.out.println("Before " + currentState);
-			if(e.getKeyCode()==KeyEvent.VK_ENTER) {
-				if(currentState == END) {
-					zombieSpawn.stop();
-manager.peashooter = new Peashooter(250, 750, 50, 50);
-					manager = new ObjectManager();
-					currentState = MENU;
-					}
-				else {
-					currentState++;
-				}
-				if(currentState == GAME) {
-					startGame();
-				}
-			}
-		
-		
-		if(e.getKeyCode()==KeyEvent.VK_SPACE && currentState == MENU) {
-			JOptionPane.showMessageDialog(null, "Use WASD to move and press space to fire");
-		}
-		
-			
-			
-		if(e.getKeyCode()==KeyEvent.VK_UP) {
-			System.out.println("UP");
-			manager.peashooter.up();
-		}
-		 
-		 
-		if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-			System.out.println("DOWN");
-			manager.peashooter.down();
-		}	 
-			if(e.getKeyCode()==KeyEvent.VK_LEFT) {
-				System.out.println("LEFT");
-				manager.peashooter.left();
-		}
-			if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
-				System.out.println("RIGHT");
-				manager.peashooter.right();
-		}
-		if(manager.peashooter.x>450) {
-			manager.peashooter.x=450;
-		}
-		if(manager.peashooter.x<0) {
-manager.peashooter.x=0; 
-		}
-		if(manager.peashooter.y>750) {
-			manager.peashooter.y=750;
-		}
-		if(manager.peashooter.y<0) {
-manager.peashooter.y=800;
-		}
-	}
-		@Override
-		public void keyReleased(KeyEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-		@Override
-		public void keyTyped(KeyEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// TODO Auto-generated method stub
-			if(currentState == MENU) {
-				updateMenuState();
-			}
-			else if(currentState == GAME) {
-				updateGameState();
-			}
-			else if(currentState == END) {
-				updateEndState();
-			}
-			//System.out.println("action");
-			repaint();
-		
-		}
-	 
-		public void paintComponent (Graphics g) {
-			if(currentState == MENU){
-			    drawMenuState(g);
-			}else if(currentState == GAME){
-			    drawGameState(g);
-			}else if(currentState == END){
-			    drawEndState(g);
-			}
-			
-		}
+for(int i=0; i<WIDTH/10 ; i++){
+	g.drawLine(i*10,0,i*10,HEIGHT);
+}
+for(int i = 0; i i<HEIGHT/10; i++){
+	g.drawLine(0,i * 10, HEIGHT, i*10);
+	
+}
+for(int i = 0; i < snake.size() ; i++) {
+	snake.get(i).draw(g);
 }
 
+@Override
+public void run() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyPressed(KeyEvent arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+}
--- src/Peashooter.java
+++ src/Peashooter.java
@@ -5,72 +5,32 @@ import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
 
-public class Peashooter extends GameObject{
-	public static BufferedImage image;
-	public static boolean needImage = true;
-	public static boolean gotImage = false;	
-	
-	Peashooter(int x, int y, int width, int height) {
-		super(x, y, width, height);
-		// TODO Auto-generated constructor stub 
-		if (needImage) {
-		    loadImage ("peashooter.png");
-		}
-		speed = 30;
+public class Peashooter {
+	private int xCoor,  yCoor, width, height;, 
+	public Peashooter(int xCoor, int yCoor , int width,int height, int tileSize) {
+		this.xCoor = xCoor;
+		this.yCoor = yCoor;
+		width = tileSize;
+		height = tileSize;
 	}
-public void draw(Graphics g) { 
-	if (gotImage) {
-		g.drawImage(image, x, y, width, height, null);
-	} else {
-		g.setColor(Color.BLUE);
-		g.fillRect(x, y, width, height);
-	}
-
-}
-		
-public void right() {
-	x+=speed;
-}
-public void left() {
-	x-=speed;
-}
-public void up() {
-	y-=speed;
-}
-public void down() {
-	y+=speed;
-} 
-
-void loadImage(String imageFile) {
-    if (needImage) {
-        try {
-            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
-	    gotImage = true;
-        } catch (Exception e) {
-            
-        }
-        needImage = false;
-    }
-}
-public static void stroke(int i, int j, int k) {
-	// TODO Auto-generated method stub
+public void tick() {
 	
 }
-public static void rect(int x, int y, int i, int j) {
-	// TODO Auto-generated method stub
-	
-}
-public static void fill(int i, int j, int k) {
-	// TODO Auto-generated method stub
-	
-}
-public static void move() {
-	// TODO Auto-generated method stub
-	
-}
-public static void size(int i, int j) {
-	// TODO Auto-generated method stub
-	
+public void draw(Graphics g){
+	g.setColor(Color.YELLOW);
+	g.fillRect(xCoor * width, yCoor* height,width,height);
 }
 
+public int getxCoor() {
+	return xCoor;
+}
+public void setxCoor(int xCoor) {
+	this.xCoor = xCoor;
+}
+public int y() {
+	return yCoor;
+}
+public void setyCoor(int yCoor) {
+	this.yCoor = yCoor;
+}
 }
